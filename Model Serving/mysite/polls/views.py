504cf9eb400dae104bcf1c8fe8681a7d4827128c
apurv121196 <apurv121196@gmail.com>
2017-12-09 20:47:46 +0530

    Django model serving files: initial commit

---
+++ Model Serving/mysite/polls/views.py
@@ -0,0 +1,76 @@
+from django.http import HttpResponse
+import tensorflow
+import pickle
+import tensorflow as tf
+from PIL import Image
+from scipy import ndimage
+import numpy as np
+
+print('Loading data...\n')
+	
+f=open('./parameters','rb')
+arr=pickle.load(f)
+print('Load complete!')
+
+def predict(X, parameters):
+    
+    W1 = tf.convert_to_tensor(parameters["W1"])
+    b1 = tf.convert_to_tensor(parameters["b1"])
+    W2 = tf.convert_to_tensor(parameters["W2"])
+    b2 = tf.convert_to_tensor(parameters["b2"])
+    W3 = tf.convert_to_tensor(parameters["W3"])
+    b3 = tf.convert_to_tensor(parameters["b3"])
+    
+    params = {"W1": W1,
+              "b1": b1,
+              "W2": W2,
+              "b2": b2,
+              "W3": W3,
+              "b3": b3}
+    
+    x = tf.placeholder("float", [196608, 1])
+    
+    z3 = forward_propagation_for_predict(x, params)
+    p = tf.argmax(z3)
+    
+    sess = tf.Session()
+    prediction = sess.run(p, feed_dict = {x: X.reshape((196608,1))})
+        
+    return prediction
+
+def forward_propagation_for_predict(X, parameters):
+    """
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+    
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
+                  the shapes are given in initialize_parameters
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3'] 
+                                                           # Numpy Equivalents:
+    Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
+    A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, a1) + b2
+    A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3,Z2) + b3
+    
+    return Z3
+
+def index(request):
+	
+	r=str(np.random.randint(5)+1)
+	image = np.array(ndimage.imread(r+'.tiff', flatten=False))
+	#my_image = scipy.misc.imresize(image, size=(64, 64)).reshape((1, 64 * 64 * 3)).T
+	my_image_prediction = predict(image, arr)
+	print("Your algorithm predicts: y = " + str(np.squeeze(my_image_prediction)))
+	return HttpResponse("For image "+r+" Your algorithm predicts: y = " + str(np.squeeze(my_image_prediction)))
